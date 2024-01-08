30b0093b76bea4f3e4e12baa2e0bf875e00785fe
nicolas_alahmar <nicolasalahmar@gmail.com>
2024-01-08 19:22:29 +0300

    log level

--- app/Aspects/LoggingAspect.php
+++ app/Aspects/LoggingAspect.php
@@ -14,28 +14,34 @@ class LoggingAspect extends Aspect
     }
 
     public function before(){
-        Log::create([
-            "operation"=>$this->message["function"],
-            "username"=>auth()->user() != null ? auth()->user()->name : "",
-            "status"=>"Started",
-            "user_id"=>auth()->user() != null ? auth()->user()->id : null,
-        ]);
+        if(in_array('Started', config('log'))){
+            Log::create([
+                "operation"=>$this->message["function"],
+                "username"=>auth()->user() != null ? auth()->user()->name : "",
+                "status"=>"Started",
+                "user_id"=>auth()->user() != null ? auth()->user()->id : null,
+            ]);
+        }
     }
     public function after(){
-        Log::create([
-            "operation"=>$this->message["function"],
-            "username"=>auth()->user() != null ? auth()->user()->name : "",
-            "status"=>"Finished",
-            "user_id"=>auth()->user() != null ? auth()->user()->id : null,
-        ]);
+        if(in_array('Finished', config('log'))){
+            Log::create([
+                "operation"=>$this->message["function"],
+                "username"=>auth()->user() != null ? auth()->user()->name : "",
+                "status"=>"Finished",
+                "user_id"=>auth()->user() != null ? auth()->user()->id : null,
+            ]);
+        }
     }
     public function exception(){
-        Log::create([
-            "operation"=>$this->message["function"],
-            "username"=>auth()->user() != null ? auth()->user()->name : "",
-            "status"=>"Exception",
-            "user_id"=>auth()->user() != null ? auth()->user()->id : null,
-        ]);
+        if(in_array('Exception', config('log'))){
+            Log::create([
+                "operation"=>$this->message["function"],
+                "username"=>auth()->user() != null ? auth()->user()->name : "",
+                "status"=>"Exception",
+                "user_id"=>auth()->user() != null ? auth()->user()->id : null,
+            ]);
+        }
     }
 
 }
